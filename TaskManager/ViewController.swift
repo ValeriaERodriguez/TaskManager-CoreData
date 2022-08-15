06ba9ef7c9b8c061af4baa8993f9fce757165f06
Valeria Rodriguez <valeriaelisabetrgz@gmail.com>
2022-08-15 13:22:15 -0300

    Initial Commit

---
+++ TaskManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskManager
+//
+//  Created by Mac on 15/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
